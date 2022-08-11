d418dba6e290116a2c4a1e02b9c9693f725614a9
plplmax <50287455+plplmax@users.noreply.github.com>
2022-08-12 02:01:01 +0300

    chore: add docker, prometheus, grafana

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,26 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+val dockerUsername: String by project
+val dockerPassword: String by project
+
 plugins {
-    kotlin("jvm") version "1.7.0"
     application
+    kotlin("jvm") version "1.7.0"
+    id("com.google.cloud.tools.jib") version "3.2.1"
 }
 
 group = "com.github.plplmax"
 version = "1.0"
+
+jib {
+    from {
+        image = "azul/zulu-openjdk-alpine:17-jre"
+        auth {
+            username = dockerUsername
+            password = dockerPassword
+        }
+    }
+}
 
 repositories {
     mavenCentral()
