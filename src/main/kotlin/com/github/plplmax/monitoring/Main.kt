64db7022449d8f49619178a38a6c6bfcd92fffc3
plplmax <50287455+plplmax@users.noreply.github.com>
2022-08-14 16:00:19 +0300

    fix: throw an exception rather suppress it

--- src/main/kotlin/com/github/plplmax/monitoring/Main.kt
+++ src/main/kotlin/com/github/plplmax/monitoring/Main.kt
@@ -3,33 +3,39 @@ package com.github.plplmax.monitoring
 import io.prometheus.client.CollectorRegistry
 import io.prometheus.client.exporter.PushGateway
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import oshi.SystemInfo
 
-fun main(): Unit = runBlocking {
-    val systemInfo = SystemInfo()
-    val gateway = PushGateway("127.0.0.1:9091")
-    val registry = CollectorRegistry.defaultRegistry
+fun main() {
+    val job = Job()
 
-    val cpu = Cpu(
-        processor = systemInfo.hardware.processor,
-        sensors = systemInfo.hardware.sensors,
-        registry = registry
-    )
+    runBlocking(job) {
+        val systemInfo = SystemInfo()
 
-    val jobName = "metrics"
-    val job = launch(Dispatchers.IO) {
-        repeat(Int.MAX_VALUE) {
-            cpu.refresh()
-            gateway.pushAdd(registry, jobName)
+        val gateway = PushGateway("127.0.0.1:9091")
+        val registry = CollectorRegistry.defaultRegistry
+
+        val cpu = Cpu(
+            processor = systemInfo.hardware.processor,
+            sensors = systemInfo.hardware.sensors,
+            registry = registry
+        )
+
+        val jobName = "metrics"
+        val refreshJob = launch(Dispatchers.IO + job) {
+            repeat(Int.MAX_VALUE) {
+                cpu.refresh()
+                gateway.pushAdd(registry, jobName)
+            }
         }
+
+        println("Enter something here to correctly stop the server...")
+        readln()
+
+        refreshJob.cancelAndJoin()
+        gateway.delete(jobName)
     }
-
-    println("Enter something here to correctly stop the server...")
-    readln()
-
-    job.cancelAndJoin()
-    gateway.delete(jobName)
 }
